95fbf175059568e93c65cdeb001614afae911985
Muhammad Shayan <shayanarif666@gmail.com>
2025-08-22 20:52:05 +0500

    Layout Images Changes

--- src/pages/TeamPage.jsx
+++ src/pages/TeamPage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Team from "../components/Team";
-import bgPlane from "/images/team/banner.avif"; // Replace with your path
+import bgPlane from "/images/acquisition/service-banner.webp"; // Replace with your path
 import Contact from "../components/Contact";
 import ScrollToTop from "../components/ScrollToTop";
 import CTABanner from "../components/CTABanner";
@@ -11,8 +11,8 @@ const TeamPage = () => {
   return (
     <>
       <section
-        className="relative w-full bg-cover bg-center z-[10]"
-        style={{ backgroundImage: `url(${bgPlane})` }}
+        className="relative w-full z-[10]"
+        style={{ backgroundImage: `url(${bgPlane})`, backgroundPosition: "center", backgroundSize: "contain !important" }}
       >
         <div className="absolute top-0 left-0 w-full h-full bg-[#111218] opacity-90 z-[-1]"></div>
         <Navbar />
